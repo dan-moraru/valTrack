7853ac58f11164f58142ea153954ddce50dce0cb
DanIsOk <danmoraru04@gmail.com>
2022-10-26 11:21:24 -0400

    big

--- web/code/js/main.js
+++ web/code/js/main.js
@@ -1,7 +1,38 @@
 let btn = document.querySelector('button');
+let user;
+let tag;
+let url;
+let ajxReq;
+let player;
 
-btn.addEventListener('click', () => {
-    let user = document.getElementById('user').value;
-    let tag = document.getElementById('name').value;
-    console.log('user: ' + user + 'tag: ' + tag);
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    user = document.getElementById('user').value;
+    tag = document.getElementById('tag').value;
+    console.log('user: ' + user + ' - tag: ' + tag);
+    url = `https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`
+    console.log(url);
+    player = requestAccount(url);
 });
+
+function requestAccount(url){
+    ajxReq = $.ajax(url, {
+        contentType : 'application/json',
+        dataType : 'json',
+        timeout : 5000,
+        
+        success: function (data, status){// success callback function
+            console.log('worked');
+            console.log(data);
+            console.log(status);
+            return data;
+        },
+
+        error: function (textStatus, errorMessage){
+            console.log('error');
+            console.log(textStatus);
+            console.log(errorMessage);
+        }
+    });
+}
+
