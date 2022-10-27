8e985b80dc34afeef092df97fa6fede83bcb713d
DanIsOk <danmoraru04@gmail.com>
2022-10-26 23:06:05 -0400

    data not parsing?

--- web/code/js/main.js
+++ web/code/js/main.js
@@ -1,7 +1,6 @@
 let btn = document.querySelector('button');
 let user;
 let tag;
-let url;
 let ajxReq;
 let player;
 
@@ -10,13 +9,17 @@ btn.addEventListener('click', (event) => {
     user = document.getElementById('user').value;
     tag = document.getElementById('tag').value;
     console.log('user: ' + user + ' - tag: ' + tag);
-    url = `https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`
-    console.log(url);
-    player = requestAccount(url);
+     
+    player = requestAccount();
+    console.log(player);
+
+    /*requestMatches(player.puuid);
+
+    getEverything(player);*/
 });
 
-function requestAccount(url){
-    ajxReq = $.ajax(url, {
+function requestAccount(){
+    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`, {
         contentType : 'application/json',
         dataType : 'json',
         timeout : 5000,
@@ -36,3 +39,70 @@ function requestAccount(url){
     });
 }
 
+let version = 'v3';
+let mode = 'matches';
+let region = 'na';
+let size = 2;
+/*
+function requestMatches(puuid){
+    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/${version}/by-puuid/${mode}/${region}/${puuid}?size=${size}`, {
+        contentType : 'application/json',
+        dataType : 'json',
+        timeout : 5000,
+        
+        success: function (data, status){// success callback function
+            console.log('worked');
+            console.log(data);
+            console.log(status);
+            return data;
+        },
+
+        error: function (textStatus, errorMessage){
+            console.log('error');
+            console.log(textStatus);
+            console.log(errorMessage);
+        }
+    });
+}
+*/
+
+/*
+function getEverything(player){
+    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`, {
+        contentType : 'application/json',
+        dataType : 'json',
+        timeout : 5000,
+        
+        success: function (data, status){// success callback function
+            console.log('worked');
+            console.log(data);
+            console.log(status);
+            player = data;
+        },
+
+        error: function (textStatus, errorMessage){
+            console.log('error');
+            console.log(textStatus);
+            console.log(errorMessage);
+        }
+    });
+
+    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/${version}/by-puuid/${mode}/${region}/${player.puuid}?size=${size}`, {
+        contentType : 'application/json',
+        dataType : 'json',
+        timeout : 5000,
+        
+        success: function (data, status){// success callback function
+            console.log('worked');
+            console.log(data);
+            console.log(status);
+            //return data;
+        },
+
+        error: function (textStatus, errorMessage){
+            console.log('error');
+            console.log(textStatus);
+            console.log(errorMessage);
+        }
+    });
+}*/
