b72e64d66a3ff6e51beaf238d910c6584e877f55
DanIsOk <danmoraru04@gmail.com>
2022-10-27 22:00:09 -0400

    yes

--- web/code/js/main.js
+++ web/code/js/main.js
@@ -4,18 +4,26 @@ let tag;
 let ajxReq;
 let player;
 
+let version = 'v3';
+let mode = 'matches';
+let region = 'na';
+let size = 2;
+let puuid;
+
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     user = document.getElementById('user').value;
     tag = document.getElementById('tag').value;
     console.log('user: ' + user + ' - tag: ' + tag);
      
-    player = requestAccount();
-    console.log(player);
+    requestAccount();
+    
+    setTimeout(function() {
+        console.log(player.data.puuid);
+        requestMatches(player.data.puuid);
+    }, 2000);
+    
 
-    /*requestMatches(player.puuid);
-
-    getEverything(player);*/
 });
 
 function requestAccount(){
@@ -24,56 +32,7 @@ function requestAccount(){
         dataType : 'json',
         timeout : 5000,
         
-        success: function (data, status){// success callback function
-            console.log('worked');
-            console.log(data);
-            console.log(status);
-            return data;
-        },
-
-        error: function (textStatus, errorMessage){
-            console.log('error');
-            console.log(textStatus);
-            console.log(errorMessage);
-        }
-    });
-}
-
-let version = 'v3';
-let mode = 'matches';
-let region = 'na';
-let size = 2;
-/*
-function requestMatches(puuid){
-    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/${version}/by-puuid/${mode}/${region}/${puuid}?size=${size}`, {
-        contentType : 'application/json',
-        dataType : 'json',
-        timeout : 5000,
-        
-        success: function (data, status){// success callback function
-            console.log('worked');
-            console.log(data);
-            console.log(status);
-            return data;
-        },
-
-        error: function (textStatus, errorMessage){
-            console.log('error');
-            console.log(textStatus);
-            console.log(errorMessage);
-        }
-    });
-}
-*/
-
-/*
-function getEverything(player){
-    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`, {
-        contentType : 'application/json',
-        dataType : 'json',
-        timeout : 5000,
-        
-        success: function (data, status){// success callback function
+        success: function (data, status){
             console.log('worked');
             console.log(data);
             console.log(status);
@@ -86,17 +45,18 @@ function getEverything(player){
             console.log(errorMessage);
         }
     });
+}
 
-    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/${version}/by-puuid/${mode}/${region}/${player.puuid}?size=${size}`, {
+function requestMatches(puuid){
+    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/${version}/by-puuid/${mode}/${region}/${puuid}?size=${size}`, {
         contentType : 'application/json',
         dataType : 'json',
         timeout : 5000,
         
-        success: function (data, status){// success callback function
+        success: function (data, status){
             console.log('worked');
             console.log(data);
             console.log(status);
-            //return data;
         },
 
         error: function (textStatus, errorMessage){
@@ -105,4 +65,26 @@ function getEverything(player){
             console.log(errorMessage);
         }
     });
-}*/
+}
+
+/*
+function getCrosshair(id){
+    ajxReq = $.ajax(`https://api.henrikdev.xyz/valorant/v1/crosshair/generate?id=${id}`, {
+        contentType : 'application/json',
+        dataType : 'json',
+        timeout : 5000,
+        
+        success: function (data, status){
+            console.log('worked');
+            console.log(data);
+            console.log(status);
+        },
+
+        error: function (textStatus, errorMessage){
+            console.log('error');
+            console.log(textStatus);
+            console.log(errorMessage);
+        }
+    });
+}
+*/
